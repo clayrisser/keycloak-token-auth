d4f0081741e64035c090112668f16c056b4945c5
Jam Risser <jam@codejam.ninja>
2020-03-25 11:22:27 +0000

    Added keycloak envs

--- src/bin.ts
+++ src/bin.ts
@@ -3,19 +3,19 @@ import KeycloakTokenAuth from './index';
 (async () => {
   const keycloakTokenAuth = new KeycloakTokenAuth(
     {
-      clientId: '',
-      name: '',
-      password: '',
-      username: ''
+      clientId: process.env.KEYCLOAK_CLIENT_ID || '',
+      name: process.env.KEYCLOAK_CLIENT_NAME || '',
+      password: process.env.KEYCLOAK_CLIENT_PASSWORD || '',
+      username: process.env.KEYCLOAK_CLIENT_USERNAME || ''
     },
     {
-      baseUrl: '',
-      realmName: ''
+      baseUrl: process.env.KEYCLOAK_BASE_URL || 'http://localhost:8080',
+      realmName: process.env.KEYCLOAK_REALM_NAME || 'master'
     }
   );
   await keycloakTokenAuth.authClient({
-    email: '',
-    token: '',
-    username: ''
+    email: process.env.KEYCLOAK_USER_EMAIL || process.argv?.[2] || '',
+    token: process.env.KEYCLOAK_USER_TOKEN || process.argv?.[3] || '',
+    username: process.env.KEYCLOAK_USER_USERNAME || process.argv?.[4] || ''
   });
 })();
